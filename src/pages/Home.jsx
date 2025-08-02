820c679f1f7e9eb7ba7bb2a5ed84b51a5a8ef9f6
natejly <natejly@gmail.com>
2025-08-02 11:01:40 -0700

    test

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="hero-section">
+      <h1>Hi, I'm Nate Ly</h1>
+      <p>Welcome to my personal website.</p>
+    </div>
+  )
+}
+
+export default Home
